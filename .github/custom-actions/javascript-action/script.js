15f29bc3bd3b8f56825d25b7686ee7fea0ae826c
jahangir <jahangirshaik24@gmail.com>
2025-07-17 10:31:35 +0530

    Javascript custom action - Inputs and deploying to S3

--- .github/custom-actions/javascript-action/script.js
+++ .github/custom-actions/javascript-action/script.js
@@ -3,7 +3,19 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 const run = () => {
-    core.notice('Hello workd from Custom Javascript action')
+    core.notice('welcome to javascript custom action')
+    exec.exec('echo', ['Deploying static web to S3'])
+
+    //Get the inputs from the action call
+    const bucketName= core.getInput('S3-BUCKET-NAME');
+    const bucketRegion= core.getInput('S3-BUCKT-REGION');
+    const buildPath= core.getInput('BUILD-LOCATION');
+
+    //Connect with S3 and upload build files
+    const s3Uri = `s3://${bucketName}`
+    exec.exec(`aws s3 sync ${buildPath} ${s3Uri} --region ${bucketRegion}`)
+    exec.exec('echo', ['Uploaded build files to S3'])
+
 };
 
 run()
