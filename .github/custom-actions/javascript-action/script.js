8fd0f9bf8807bbea01eff38645b1f46e8909ef99
jahangir <jahangirshaik24@gmail.com>
2025-07-17 11:23:56 +0530

    javascript custom action

--- .github/custom-actions/javascript-action/script.js
+++ .github/custom-actions/javascript-action/script.js
@@ -5,20 +5,17 @@ const exec = require('@actions/exec');
 const run = () => {
     core.notice('welcome to javascript custom action')
     exec.exec('echo', ['Deploying static web to S3'])
-
     //Get the inputs from the action call
     const bucketName= core.getInput('S3-BUCKET-NAME');
     const bucketRegion= core.getInput('S3-BUCKT-REGION');
     const buildPath= core.getInput('BUILD-LOCATION');
-
-    //Connect with S3 and upload build files
+    //Connect with S3 and upload build files - aws command
     const s3Uri = `s3://${bucketName}`
     exec.exec(`aws s3 sync ${buildPath} ${s3Uri} --region ${bucketRegion}`)
     exec.exec('echo', ['Uploaded build files to S3'])
-
+    //To return the output from the action
     const hostedUrl = `http://${bucketName}.s3-website.${bucketRegion}.amazonaws.com`;
     core.setOutput('hosted-url', hostedUrl);
-
 };
 
-run()
+run();
